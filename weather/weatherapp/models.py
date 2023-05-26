dc6e93fff40dedcab074aa7c08b6f11a3064b822
Pooja Gawade <104082200+OKPooja@users.noreply.github.com>
2023-05-26 11:35:52 +0530

    Update models.py

--- weather/weatherapp/models.py
+++ weather/weatherapp/models.py
@@ -5,4 +5,7 @@ class City(models.Model):
     name = models.CharField(max_length= 45)
     
     class Meta:
-        verbose_name_plural = 'cities'
+        verbose_name_plural = 'cities'  #it just modifies the respective object name in admin panel for plural objects
+                                        #for example instead of Citys it will be cities now in the admin panel
+                                        #django be defaults just add s to the class name to make it plural ig
+                                        #so by using this we can change that
