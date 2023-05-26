6d9d0cc9f701e5ebefbcafd0bcef614203405e3e
Pooja Gawade <104082200+OKPooja@users.noreply.github.com>
2023-05-26 11:24:24 +0530

    Update models.py

--- weather/weatherapp/models.py
+++ weather/weatherapp/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class City(models.Model):
     name = models.CharField(max_length= 45)
+    
+    class Meta:
+        verbose_name_plural = 'cities'
